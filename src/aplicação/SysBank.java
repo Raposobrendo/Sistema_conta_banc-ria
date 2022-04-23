fbe8c1e62cae0e867556dd228bfcdaa9541f9944
Brendo Raposo <raposobrendo@gmail.com>
2022-04-23 18:50:21 -0300

    correção main

--- src/aplicação/SysBank.java
+++ src/aplicação/SysBank.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 import entidade.Cliente;
 
 public class SysBank {
-	public static void SysBanc() {
+	public static void main(String[] Args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		Cliente cliente = new Cliente();
